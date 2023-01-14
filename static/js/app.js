447f736289239358b3fe75d63199d6ecc7c21f80
Robert Franklin <robertlsfranklin@gmail.com>
2023-01-14 11:08:01 +0800

    finished app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,64 +1,94 @@
-// Step 1: Read in the JSON data and parse it
-const data = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
-d3.json(data).then(function(data) {
-  // Step 2: Create an array of objects, where each object represents an OTU
-  var otus = data.samples.map(function(sample) {
-    return {
-      otu_id: sample.otu_ids,
-      sample_value: sample.sample_values,
-      otu_label: sample.otu_labels
-    };
-  });
+// Function for building charts
+function buildChart(id) {
 
-  // Step 3: Sort the array of objects by sample_value, in descending order
-  otus.sort(function(a, b) {
-    return b.sample_value - a.sample_value;
-  });
+  // load data from JSON file
+  d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then(data => {
+    
+  // extract the top 10 OTUs for the selected id
+    var samples = data.samples;
+    var filtered_samples = samples.filter(sample => sample.id === id);
+    var otu_ids = filtered_samples[0].otu_ids.slice(0, 10);
+    var sample_values = filtered_samples[0].sample_values.slice(0, 10);
+    var otu_labels = filtered_samples[0].otu_labels.slice(0, 10);
 
-  // Print the sorted array of OTUs to the console to verify that it was sorted correctly
-  console.log(otus);
-
-  // Step 4: Create a dropdown menu with a list of all of the individuals
-  var select = d3.select("#selDataset");
-
-  select.selectAll("option")
-    .data(data.names)
-    .enter()
-    .append("option")
-      .attr("value", function(d) { return d; })
-      .text(function(d) { return d; });
-
-  // Step 4a: Add an onchange event listener to the select element
-  select.on("change", function() {
-    // Get the selected individual from the dropdown menu
-    var selectedIndividual = d3.select(this).property("value");
-    optionChanged(selectedIndividual);
-  });
-
-  // Define the optionChanged function
-  function optionChanged(selectedIndividual) {
-    // Filter the array of OTUs to only include the top 10 for the selected individual
-    var top10otus = otus.filter(function(otu) {
-      return otu.otu_id.includes(selectedIndividual);
-    }).slice(0, 10);
-
-    // Update the bar chart with the new data
+    // create the bar chart using Plotly
     var trace = {
-      x: top10otus.map(function(otu) { return otu.sample_value; }),
-      y: top10otus.map(function(otu) { return otu.otu_id; }),
+      x: sample_values,
+      y: otu_ids.map(id => `OTU ${id}`),
+      text: otu_labels,
       type: "bar",
-      orientation: "h",
-      text: top10otus.map(function(otu) { return otu.otu_label; })
+      orientation: "h"
     };
-
     var layout = {
-      title: "Top 10 OTUs for Selected Individual",
+      title: "Top 10 OTUs per Individual",
       yaxis: {
         autorange: "reversed"
       }
     };
-
     Plotly.newPlot("bar", [trace], layout);
-  }
+
+    // create the bubble chart using Plotly
+    var trace = {
+      x: filtered_samples[0].otu_ids,
+      y: filtered_samples[0].sample_values,
+      mode: 'markers',
+      marker: {
+        size: filtered_samples[0].sample_values,
+        color: filtered_samples[0].otu_ids
+      },
+      text: filtered_samples[0].otu_labels,
+    };
+
+    var layout = {
+      title: "Bubble Chart of all samples",
+      xaxis: {title: "OTU ID"},
+      yaxis: {title: "Sample Values"}
+    };
+
+    Plotly.newPlot("bubble", [trace], layout);
+  
+    // extract the metadata for the selected id
+    var metadata = data.metadata;
+    var filtered_metadata = metadata.filter(md => md.id == id);
+
+    // select the #sample-metadata element
+    var sampleMetadata = d3.select("#sample-metadata");
+
+    // clear any existing metadata
+    sampleMetadata.html("");
+
+  // loop over the properties of the filtered metadata object and append a new element for each key-value pair
+  Object.entries(filtered_metadata[0]).forEach(([key, value]) => {
+    var newElement = sampleMetadata.append("div");
+    newElement.html(`<strong>${key}:</strong> ${value}`);
+  });
+
+})};
+
+// select the select element
+var select = d3.select("#selDataset");
+
+//load data from json file
+d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then(data => {
+  
+// extract the list of ids from the data
+
+  var ids = data.names;
+  
+  // add options to the select element
+  ids.forEach(id => {
+    var option = select.append("option");
+    option.text(id);
+  });
+  
+  // set the first ID as the default value
+  select.property("value", ids[0]);
+  
+  // call the buildChart function with the first ID in the list as an argument
+  buildChart(ids[0]);
 });
 
+// Define the optionChanged formula
+function optionChanged(id) {
+  buildChart(id);
+}
